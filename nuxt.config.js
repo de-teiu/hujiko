5b88bdb09465cf1d53a84439476fea27523d0aef
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2019-03-19 23:53:52 +0900

    twitterカード設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,52 +4,85 @@ export default {
   mode: 'spa',
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        "http-equiv": "X-UA-Compatible",
+        content: "IE=edge"
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: pkg.description
+      }, {
+        name: "twitter:card",
+        content: "summary"
+      },
+      {
+        name: "twitter:site",
+        content: "@de_teiu_tkg"
+      },
+      {
+        name: "twitter:title",
+        content: "くぁwせdrftgyふじこlp；＠：「」"
+      },
+      {
+        name: "twitter:description",
+        content: "くぁwせdrftgyふじこlp；＠：「」をツイートまたはクリップボードにコピーするアプリです."
+      },
+      {
+        name: "twitter:image",
+        content: "https://hujiko.netlify.com/hujiko.png"
+      }
+
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#fff'
+  },
 
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
 
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     '@nuxtjs/pwa',
   ],
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
 }
